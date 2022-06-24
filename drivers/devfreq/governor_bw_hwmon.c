5a725ebc52958a80a060fa212d634735e40d425b
Jaran Kepang <pribadi228@gmail.com>
2022-06-24 19:50:36 +0700

    Revert "PM / devfreq: make memlat and bwmon governors immutable"
    This reverts commit d4cbc1c2fc228d38df67064ff18170e649cf5cbd.

--- drivers/devfreq/governor_bw_hwmon.c
+++ drivers/devfreq/governor_bw_hwmon.c
@@ -613,11 +613,6 @@ static int gov_start(struct devfreq *df)
 		goto err_sysfs;
 	}
 
-	mutex_lock(&df->lock);
-	df->min_freq = df->max_freq;
-	update_devfreq(df);
-	mutex_unlock(&df->lock);
-
 	return 0;
 
 err_sysfs:
@@ -940,7 +935,6 @@ out:
 
 static struct devfreq_governor devfreq_gov_bw_hwmon = {
 	.name = "bw_hwmon",
-	.immutable = 1,
 	.get_target_freq = devfreq_bw_hwmon_get_freq,
 	.event_handler = devfreq_bw_hwmon_ev_handler,
 };
